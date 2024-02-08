320b578e05fa5fd880a8ca6ece2f7e0d85a624ac
sadikhasansaddam <sadikhasansaddam46@gmail.com>
2024-02-09 05:15:20 +0600

    sliding cart added

---
+++ js/cart.js
@@ -0,0 +1,12 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeBtn = document.querySelector('.cartTab .close');
+let body = document.querySelector('body');
+
+
+    // open and close tab
+    iconCart.addEventListener('click', () => {
+        body.classList.toggle('activeTabCart');
+    })
+    closeCart.addEventListener('click', () => {
+        body.classList.toggle('activeTabCart');
+    })
