ffd5d4f2cf777dddff7da5bf22a8ab574097f926
sadikhasansaddam <sadikhasansaddam46@gmail.com>
2024-02-09 05:29:27 +0600

    sidebar adjusted

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,28 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+
+
 let iconCart = document.querySelector('.icon-cart');
 let closeBtn = document.querySelector('.cartTab .close');
 let body = document.querySelector('body');
@@ -10,3 +35,6 @@ let body = document.querySelector('body');
     closeBtn.addEventListener('click', () => {
         body.classList.toggle('activeTabCart');
     })
+
+
+
