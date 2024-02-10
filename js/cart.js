b6aa5981b718bfff58b927369077dcd35f082b47
sadikhasansaddam <sadikhasansaddam46@gmail.com>
2024-02-10 14:56:44 +0600

    adding template into the homepage

--- js/cart.js
+++ js/cart.js
@@ -20,7 +20,6 @@ menuBar.addEventListener('click', function () {
 	sidebar.classList.toggle('hide');
 })
 
-
 const searchButton = document.querySelector('#content nav form .form-input button');
 const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
 const searchForm = document.querySelector('#content nav form');
@@ -38,9 +37,6 @@ searchButton.addEventListener('click', function (e) {
 })
 
 
-
-
-
 if(window.innerWidth < 768) {
 	sidebar.classList.add('hide');
 } else if(window.innerWidth > 576) {
@@ -48,14 +44,12 @@ if(window.innerWidth < 768) {
 	searchForm.classList.remove('show');
 }
 
-
 window.addEventListener('resize', function () {
 	if(this.innerWidth > 576) {
 		searchButtonIcon.classList.replace('bx-x', 'bx-search');
 		searchForm.classList.remove('show');
 	}
 })
-
 
 
 const switchMode = document.getElementById('switch-mode');
@@ -76,7 +70,7 @@ let closeBtn = document.querySelector('.cartTab .close');
 let body = document.querySelector('body');
 
 
-    // open and close tab
+    // open and close cart tab
     iconCart.addEventListener('click', () => {
         body.classList.toggle('activeTabCart');
     })
